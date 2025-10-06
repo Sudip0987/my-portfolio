aa3e7a3697669988467bcf6c535a6ed11ed5a49f
Sudip Sharma <sudeepsharma17@gmail.com>
2025-10-07 10:45:06 +1100

    About section created with intro and skills

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Projects from "./components/Projects"
+
+
 
 function App() {
   return (
     <div>
       <Header />
       <Hero/>
+     <About/>
       {/* Other sections will go here later */}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">Your Name</div>
+    <header className="bg-gray-800 text-white p-4 fixed w-full top-0 z-50">
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-4">
+        {/* Logo / Name */}
+        <div className="text-2xl font-bold">Sudip Sharma</div>
+
+        {/* Navigation */}
         <nav>
-          <ul className="flex space-x-6">
+          <ul className="flex space-x-6 text-lg">
             <li><a href="#home" className="hover:text-yellow-400">Home</a></li>
             <li><a href="#about" className="hover:text-yellow-400">About</a></li>
             <li><a href="#projects" className="hover:text-yellow-400">Projects</a></li>
