1b7aa5bd90090305d91a6a8882af4eda17456a3b
Sudip Sharma <sudeepsharma17@gmail.com>
2025-10-08 19:14:14 +1100

    Updated Navbar with animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,13 +28,24 @@ function Header() {
         {isOpen ? CloseIcon : HamburgerIcon}
       </button>
         {/* Navigation */}
-        <nav className={`${isOpen ? "block" : "hidden"} md:flex flex-wrap space-x-6 text-lg absolute md:static top-16 left-0 w-full md:w-auto bg-gray-800 mdbg-transparent`}>
+        <nav className={`${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"} md:flex md:max-h-full md:opacity-100  md:flex flex-col md:flex-row md:space-x-6 text-lg absolute md:static top-16 left-0 w-full md:w-auto bg-gray-800 md:bg-transparent overflow-hidden transition-all duration-200 ease-in-out`}>
 
-          <ul className="flex flex-col mdfkex-row md:space-x-6">
-            <li><a href="#home" className="hover:text-yellow-400" onClick={() => setIsOpen(false)}>Home</a></li>
-            <li><a href="#about" className="hover:text-yellow-400" onClick={() => setIsOpen(false)}>About</a></li>
-            <li><a href="#projects" className="hover:text-yellow-400" onClick={() => setIsOpen(false)}>Projects</a></li>
-            <li><a href="#contact" className="hover:text-yellow-400" onClick={() => setIsOpen(false)}>Contact</a></li>
+        <ul className="flex flex-col md:flex-row md:space-x-6 items-center justify-center p-4 md:p-0">
+        <li>
+  <a 
+    href="#home" 
+    className="hover:text-yellow-400 px-4 py-2 rounded transform transition duration-200 ease-in-out hover:scale-110 inline-block" 
+    onClick={() => setIsOpen(false)}
+  >
+    Home
+  </a>
+</li>        <li><a href="#about" 
+    className="hover:text-yellow-400 px-4 py-2 rounded transform transition duration-200 ease-in-out hover:scale-110 inline-block" 
+    onClick={() => setIsOpen(false)}>About</a></li>
+            <li><a href="#projects"     className="hover:text-yellow-400 px-4 py-2 rounded transform transition duration-200 ease-in-out hover:scale-110 inline-block" 
+ onClick={() => setIsOpen(false)}>Projects</a></li>
+            <li><a href="#contact"     className="hover:text-yellow-400 px-4 py-2 rounded transform transition duration-200 ease-in-out hover:scale-110 inline-block" 
+ onClick={() => setIsOpen(false)}>Contact</a></li>
           </ul>
         </nav>
       </div>
