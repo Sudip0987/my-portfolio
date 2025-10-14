24570612c34768a1dd7d3058610c0476f1627fa2
Sudip Sharma <sudeepsharma17@gmail.com>
2025-10-14 11:03:05 +1100

    move ExperienceCard component to its own file for reusebaility

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,16 @@
 import React from "react";
+import ExperienceCard from "./ExperienceCard";
+
+const experiences = [
+    {
+    title:"test",
+    description:"desc",
+    image:"/images/screenshots/supporteddecisionmaking.png",
+    technologies:["HTML/CSS", "PHP"],
+    link:"s"
+    }
+
+];
 
 function Experience() {
     return (
@@ -6,24 +18,22 @@ function Experience() {
             <div className="mx-auto "  >
                 <h2 className="text-4xl text-center font-bold mb-6s">Past experience</h2>
                 {/*Experience cards*/}
+              
+              {experiences.map((exp,index)=>(
+                <ExperienceCard
+                key={index}
+                title= {exp.title}
+                description={exp.description}
+                image={exp.image}
+                technologies={exp.technologies}
+                link={exp.link}
+                />
+              ))}
+              
 
-                <div className="my-10 max-w-4xl mx-auto flex flex-col md:flex-row items-stretch p-5 gap-6 rounded-md border border-gray-300/70 shadow-md" >
-                    <img className="w-full md:w-2/3 h-80 object-cover rounded-lg" alt="Supported decision-making project"
-                        src="/images/screenshots/supporteddecisionmaking.png"></img>
-                    <div className="flex flex-col">
-                        <h3 className="text-2xl mb-5" > Custom-Built Accessible WordPress Website</h3>
-                        <p className="text-gray-700 mb-4">A custom-built WordPress website designed to assist individuals with disabilities in decision-making. Developed mainly using HTML, CSS, and PHP, with features like dark mode, login, and signup. My contributions focused on backend functionality and frontend design to create a responsive, user-friendly experience.</p>
-                        <div className="flex flex-wrap">
-                            <span className="inline-block bg-purple-100 text-purple-800 px-3 py-1 rounded-full text-sm mr-1">HTML/CSS</span>
-
-                            <span className="inline-block rounded-full bg-purple-100  text-purple-800 px-3 py-1 text-sm">PHP</span>
-                        </div>
-
-                         <a href="https://decidingwithsupport.flinders.edu.au/" target="_blank" rel="noopener noreferrer"className="inline-block  text-center mt-4 bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 transition">Live demo</a>
-
-                    </div>
-
-                </div>
+               
+           
+                
             </div>
         </section>
     );
---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function ExperienceCard({title, description, image, technologies, link}){
+
+    return(
+  <div className="my-10 max-w-4xl mx-auto flex flex-col md:flex-row items-stretch p-5 gap-6 rounded-md border border-gray-300/70 shadow-md" >
+                    <img className="w-full md:w-2/3 h-80 object-cover rounded-lg" alt="Supported decision-making project"
+                        src="/images/screenshots/supporteddecisionmaking.png"></img>
+                    <div className="flex flex-col">
+                        <h3 className="text-2xl mb-5" > {title}</h3>
+<                       p className="text-gray-700 mb-4">{description}</p>
+                        <div className="flex flex-wrap">
+                            {technologies.map((tech, index)=>(
+                            <span key={index}
+                            className="inline-block bg-purple-100 text-purple-800 px-3 py-1 rounded-full text-sm mr-1">
+                                {tech}
+                                </span>
+
+                            ))}
+
+                        </div>
+
+                         <a href={link} target="_blank" rel="noopener noreferrer"className="inline-block  text-center mt-4 bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 transition">Live demo</a>
+
+                    </div>
+
+                </div>
+
+                   );
+}
+export default ExperienceCard;
