4909cf0121fc87460a1752b6db9d3acb0ff981b4
Sudip Sharma <sudeepsharma17@gmail.com>
2025-10-14 11:34:14 +1100

    Changed Experience card layout to vertical for consistent sizing and better responsiveness

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -3,11 +3,12 @@ import React from "react";
 function ExperienceCard({title, description, image, technologies, link}){
 
     return(
-  <div className="my-10 max-w-4xl mx-auto flex flex-col md:flex-row items-stretch p-5 gap-6 rounded-md border border-gray-300/70 shadow-md" >
-                    <img className="w-full md:w-2/3 h-80 object-cover rounded-lg" alt="Supported decision-making project"
+  <div className="my-10 max-w-4xl mx-auto flex flex-col items-stretch p-5 gap-2 rounded-md border border-gray-300/70 shadow-md" >
+                    
+                                                <h3 className="text-2xl mb-5" > {title}</h3>
+
+                        <img className="w-full md:w-2/3 h-80 object-cover rounded-lg" alt="Supported decision-making project"
                         src={image}></img>
-                    <div className="flex flex-col">
-                        <h3 className="text-2xl mb-5" > {title}</h3>
 <                       p className="text-gray-700 mb-4">{description}</p>
                         <div className="flex flex-wrap">
                             {technologies.map((tech, index)=>(
@@ -20,9 +21,8 @@ function ExperienceCard({title, description, image, technologies, link}){
 
                         </div>
 
-                         <a href={link} target="_blank" rel="noopener noreferrer"className="inline-block  text-center mt-4 bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 transition">Live demo</a>
+                         <a href={link} target="_blank" rel="noopener noreferrer"className="inline-block w-32  text-center mt-4 bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 transition">Live demo</a>
 
-                    </div>
 
                 </div>
 
